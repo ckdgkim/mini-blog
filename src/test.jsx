632aff42c646316c744af10740dea71d1231d9c3
kostackdgk <kostackdgkim@naver.com>
2024-11-22 10:36:24 +0900

    first commit

---
+++ src/test.jsx
@@ -0,0 +1,16 @@
+// function App() {
+//     const [ count, setCount ] = useState(0)
+//     const onClick = () => {
+//         setCount(count + 1)
+//     }
+
+//     return (
+//         <>
+//             <h1>New Counter using Reducer</h1>
+//             <p>{count}</p>
+//             <button> onClick={onClick} = useState(0)
+                
+//             </button>
+//         </>
+//     )
+// }
